a432f0d9aa7ad1b32e9c00cab27514cbf02e4c9b
Emmanuel Wilson <emmanuelwilson@Emmanuels-MacBook.local>
2021-09-11 16:19:10 +0530

    .

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -5,8 +5,8 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
-    return <button className='ui violet button'>Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Bhejo';
+    return <button className='ui violet button'>{text}</button>;
   }
 }
 
--- src/Components/Field.js
+++ src/Components/Field.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text =
+      this.context === 'english'
+        ? "what's is your name :"
+        : 'Naam kya hai tumhara: ';
+
     return (
       <div className='ui field'>
-        <label>Name:</label>
+        <label>{text}</label>
         <input />
       </div>
     );
